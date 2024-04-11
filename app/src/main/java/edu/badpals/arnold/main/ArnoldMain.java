0d389b65c73101c28af87b389e59b1b669b3ae96
mdss00 <miguids00@gmail.com>
2024-04-11 10:36:01 +0200

    feat (ArnoldMain) : add last part of the main

--- app/src/main/java/edu/badpals/arnold/main/ArnoldMain.java
+++ app/src/main/java/edu/badpals/arnold/main/ArnoldMain.java
@@ -18,5 +18,10 @@ public class ArnoldMain {
         for(Planeta planeta: Planeta.getPlanetasTerrestres()){
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
         }
+
+        System.out.println("\nYour weight only on the gas giant planets: ");
+        for(Planeta planeta: Planeta.getGigantesGaseosos()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
     }
 }
